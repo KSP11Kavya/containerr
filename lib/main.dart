71f44b5242985e6aa91200308ca8a99e749645b1
kavyapandey <kavyapandeyksp11@gmail.com>
2023-11-02 17:51:09 +0530

    container learning

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,24 @@
-//1. child:  Container widget has a property ‘child:’
-// which stores its children.
-// The child class can be any widget.
-// Let us take an example, taking a text widget as a child.
-// import 'package:flutter/material.dart';
-//
-// void main() {
-//   runApp(MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text("My Container example"),
-//         ),
-//         body: Container(
-//           child: Text(
-//             "Hello, i am inside a container!",
-//             style: TextStyle(fontSize: 20)),
-//         ),
-//       ),
-//     );
-//   }
-// }
-//2. color:  The color property sets the background color of the entire container.
-// Now we can visualize the position of the container using a
-// background color.
-// import 'package:flutter/material.dart';
-//
-// void main() {
-//   runApp(MyApp());
-// }
-// class MyApp extends StatelessWidget {
-//
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(title: Text("example"),),
-//         body: Container(
-//           color: Colors.purple,
-//           child: Text("Hello i am inside"),
-//           style: TextStyle(fontSize: 20)
-//         ),
-//       ),
-//     )
-//   }
-// }
-//3. height and width: By default,
-//a container class takes the space that is required by the child.
-//We can also specify the height and width of the container based on our requirements.
 import 'package:flutter/material.dart';
 
-void main() => runApp( MyApp());
+void main() => runApp(MyApp());
+//1. Child: The 'child:' attribute of the
+// Container widget is used to store the widget's children.
+// Any widget may be used as the child class.
+// Consider a text widget as an example.
 
 class MyApp extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title:  Text("Container example"),
+          title: 'Container App',
         ),
         body: Container(
-          height: 200,
-          width: double.infinity,
-          color: Colors.purple,
-          child:  Text("Hello! i am inside a container!",
-              style: TextStyle(fontSize: 20)),
+          child: Text(
+            "i am inside container",
+            style: TextStyle(fontSize: 23),
+          ),
         ),
       ),
     );
